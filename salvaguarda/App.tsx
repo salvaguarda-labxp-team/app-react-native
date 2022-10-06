eb31e8304e7676c103710eedba64f81029559c6b
Vitor Terra <vitortterra@gmail.com>
2022-10-06 15:33:02 -0300

    wip

--- salvaguarda/App.tsx
+++ salvaguarda/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import  LoginScreen from './screens/LoginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginScreen />
     </View>
   );
 }
---
+++ salvaguarda/screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10,
+    },
+});
