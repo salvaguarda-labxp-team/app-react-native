836577445dc3dd2c23228d9d381f66f59b77046d
Vitor Terra <vitortterra@gmail.com>
2022-10-06 16:28:50 -0300

    add basic form template from react hook form

--- salvaguarda/screens/LoginScreen.js
+++ salvaguarda/screens/LoginScreen.js
@@ -1,23 +1,85 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { Alert, Button, Text, View, TextInput, StyleSheet } from 'react-native';
+import { useForm, Controller } from "react-hook-form";
+
 
 const LoginScreen = () => {
-    return (
-        <View style={styles.container}>
-        </View>
-    );
-};
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: ''
+    }
+  });
+
+  const onSubmit = data => console.log(data);
+
+  return (
+    <View>
+    <Controller
+        control={control}
+        rules={{
+        required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+        <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+        />
+        )}
+        name="firstName"
+    />
+    {errors.firstName && <Text>This is required.</Text>}
+
+    <Controller
+        control={control}
+        rules={{
+        maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+        <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+        />
+        )}
+        name="lastName"
+    />
+
+    <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+  </View>
+)};
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    button: {
-        width: 200,
-        marginTop: 10,
-    },
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 10,
-    },
+  label: {
+    color: 'white',
+    margin: 20,
+    marginLeft: 0,
+  },
+  button: {
+    marginTop: 40,
+    color: 'white',
+    height: 40,
+    backgroundColor: '#ec5990',
+    borderRadius: 4,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    // paddingTop: Constants.statusBarHeight,
+    padding: 8,
+    backgroundColor: '#0e101c',
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: 'none',
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+  },
 });
