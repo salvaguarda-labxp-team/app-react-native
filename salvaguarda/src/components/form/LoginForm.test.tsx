da2fb6d1d471cca0708c252df6093981220612d6
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-10-21 18:13:08 -0300

    feat: add tests to RedirectToForgotPasswordLink

--- salvaguarda/src/components/form/LoginForm.test.tsx
+++ salvaguarda/src/components/form/LoginForm.test.tsx
@@ -1,15 +1,34 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { render, screen, fireEvent } from '@testing-library/react-native';
 
 import { RedirectToForgotPasswordLink } from "./LoginForm";
 
 describe("RedirectToForgotPasswordLink", () => {
   it("Renders 1 child on React tree", () => {
     const mockFn = jest.fn()
-    const component = renderer.create(<RedirectToForgotPasswordLink redirectToForgotPassword={mockFn} />)
+    const component = render(<RedirectToForgotPasswordLink redirectToForgotPassword={mockFn} />)
     const tree = component.toJSON();
     // @ts-ignore
     expect(tree.children.length).toBe(1);
   });
+  it("Renders text element with forgot password text", () => {
+    const mockFn = jest.fn()
+    render(<RedirectToForgotPasswordLink redirectToForgotPassword={mockFn} />)
+    const textElement =  screen.getByText('Esqueci minha senha')
+    expect(textElement).toBeTruthy()
+  });
+  it("Only by clicking the text element, executes 'redirectToForgotPassword' function", () => {
+    const mockFn = jest.fn()
+    render(<RedirectToForgotPasswordLink redirectToForgotPassword={mockFn} />)
+    const textElement =  screen.getByText('Esqueci minha senha')
+    // before firing event, function may not be called
+    expect(mockFn).not.toBeCalled()
+    
+    fireEvent.press(textElement)
+    
+    // after firing event, function MUST be called
+    expect(mockFn).toBeCalled()
+  });
+  
 });
 
