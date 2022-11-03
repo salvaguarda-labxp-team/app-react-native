2e97e38b402163203d295e9be0a2e86f4fca5553
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-11-03 18:04:10 -0300

    refactor: lint, extract displayItems and displayImages

--- src/views/addImage.tsx
+++ src/views/addImage.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Image, View, Alert } from "react-native";
-import * as ImagePicker from "expo-image-picker";
+import React, { useCallback, useMemo } from "react";
+import { Image, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import Swiper from "react-native-swiper";
 
-export default function ImagePickerExample(props): JSX.Element {
-  const [selectedImages, setSelectedImages] = useState(null);
-
+const ImagePickerExample = (props: any): JSX.Element => {
   // const openCamera = async (): Promise<void> => {
   //   // Ask the user for the permission to access the camera
   //   const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
@@ -24,100 +21,103 @@ export default function ImagePickerExample(props): JSX.Element {
   //   }
   // };
 
-  const save = async (): Promise<void> => {
-    Alert.alert("Imagem adicionada!");
+  const cancelSelection = async (): Promise<void> => {
+    // TODO implement selection cancelation
+    console.log(displayItems);
   };
 
-  const test = async (): Promise<void> => {
-   console.log(selectedImages);
-  };
+  const pickImage = useCallback(() => {
+    // TODO implement open image selection
+    console.log("oi");
+  }, []);
 
-  // const addImage = async (): Promise<void> => {
+  
+  const displayImages = useMemo(
+    () =>
+      (props.route.params.images as string[]).map((newImage) => (
+        <View style={{ width: "19%", height: "100%", marginRight: "1.25%" }}>
+          <Image
+            source={{ uri: newImage }}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+      )),
+    [props?.route?.params?.images]
+  );
 
-  //   let images: any[] = [];
-
-  //     props.route.params.images.forEach( (newImage: string) =>
-  //       images.push(
-  //         <View>
-  //           <Image
-  //                     source={{ uri: newImage }}
-  //                     style={{ width: '10%', height: '10%'}}
-  //                   />
-  //         </View>
-  //       )
-  //     )
-  //     setSelectedImages(images);
-  // };
-  useEffect( () => {
-      let images: any[] = [];
-
-        props.route.params.images.forEach( (newImage: string) =>
-          images.push(
-            <View style={{ width: '19%', height: '100%', marginRight: '1.25%'}}>
-              <Image
-                        source={{ uri: newImage }}
-                        style={{ width: '100%', height: '100%'}}
-                      />
-            </View>
-          )
-        )
-
-        if(images.length < 5){
-          images.push(
-            <View style={{ width: '19%', height: '100%', marginRight: '1.25%', backgroundColor: 'rgba(0,0,0,0.8)', alignItems: 'center', justifyContent:'center' }}>
-              <MaterialIcons
-                name="add-to-photos"
-                size={40}
-                color="white"
-                //onPress={pickImage}
-              />
-            </View>
-          )
-        }
-        setSelectedImages(images);
-    }, []
-  )
-
+  const displayItems = useMemo(() => {
+    return displayImages.length > 5
+      ? displayImages
+      : [
+          ...displayImages,
+          <View
+            style={{
+              width: "19%",
+              height: "100%",
+              marginRight: "1.25%",
+              backgroundColor: "rgba(0,0,0,0.8)",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <MaterialIcons
+              name="add-to-photos"
+              size={40}
+              color="white"
+              onPress={pickImage}
+            />
+          </View>,
+        ];
+  }, [displayImages]);
 
   return (
     <View style={{ flex: 1 }}>
       <StatusBar hidden />
-      
+
       <Swiper>
         <Image
           source={{ uri: props.route.params.images[0] }}
-          style={{ width: '100%', height: '100%', shadowOpacity: 0 }}
+          style={{ width: "100%", height: "100%", shadowOpacity: 0 }}
         />
 
         <Image
           source={{ uri: props.route.params.images[1] }}
-          style={{ width: '100%', height: '100%', shadowOpacity: 0 }}
+          style={{ width: "100%", height: "100%", shadowOpacity: 0 }}
         />
-        
       </Swiper>
 
-      <View style={{ position: "absolute", top: 20, left: 20, shadowOpacity: 1 }}>
-        <MaterialIcons name="cancel" size={40} color="white" onPress={test} />
+      <View
+        style={{ position: "absolute", top: 20, left: 20, shadowOpacity: 1 }}
+      >
+        <MaterialIcons
+          name="cancel"
+          size={40}
+          color="white"
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onPress={cancelSelection}
+        />
       </View>
 
-      <View style={{ position: "absolute", top: 20, right: 20, shadowOpacity: 1 }}>
+      <View
+        style={{ position: "absolute", top: 20, right: 20, shadowOpacity: 1 }}
+      >
         <MaterialIcons name="restore-from-trash" size={40} color="white" />
       </View>
 
-      <View style={{ position: "absolute", bottom: 0, width: '100%', height: '10%', backgroundColor: 'rgba(0,0,0,0.4)', flexDirection: "row" }}>
-        {selectedImages}
+      <View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: "10%",
+          backgroundColor: "rgba(0,0,0,0.4)",
+          flexDirection: "row",
+        }}
+      >
+        {displayItems}
       </View>
-
-
-      {/* <View style={{ position: "absolute", bottom: 20, left: 20 }}>
-        <MaterialIcons
-          name="add-box"
-          size={30}
-          color="black"
-          onPress={openCamera}
-        />
-      </View> */}
-
     </View>
   );
-}
+};
+
+export default ImagePickerExample;
