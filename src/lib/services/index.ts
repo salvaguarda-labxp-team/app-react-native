75691fb6fff2e899474d9a452b9b2d998d759161
Arthur Prado De Fazio <arthur.fazio@usp.br>
2022-11-07 19:21:10 -0300

    Reescreve modal com react-native-modal

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -1,3 +1,4 @@
 export * from "./AuthenticationAPI";
 export * from "./MessagesAPI";
 export * from "./RoomsAPI";
+export * from "./QuestionsAPI";
--- src/views/QuestionsListScreen.tsx
+++ src/views/QuestionsListScreen.tsx
@@ -1,23 +1,112 @@
 import { useState } from "react";
 import DropDownPicker from 'react-native-dropdown-picker';
-import { Text, View, StyleSheet, Modal, Pressable } from "react-native";
+import Modal from "react-native-modal";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 import { Input, ButtonGroup, FAB} from "react-native-elements";
 import { MaterialIcons } from "@expo/vector-icons";
+import { AuthenticationAPI, QuestionsAPI } from "../lib/services";
+
+export default function QuestionsListScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [description, setDescription] = useState("");
+  const [questionTitle, setQuestionTitle] = useState("");
+  const [dropDrownExpanded, setDropDownExpanded] = useState(false);
+  const [chosenSubject, setChosenSubject] = useState("");
+  const [availableSubjects, setAvailableSubjects] = useState([
+    { label: "Matemática", value: "Math" },
+    { label: "Português", value: "Port" },
+    { label: "Geografia", value: "Geo" },
+    { label: "Biologia", value: "Bio" },
+    { label: "Química", value: "Chem" },
+    { label: "História", value: "Hist"},
+    { label: "Filosofia", value: "Philo"},
+    { label: "Sociologia", value: "Socio"},
+    { label: "Física", value: "Phys"},
+    { label: "Artes", value: "Arts"}
+  ])
+
+  const createQuestion = async () => {
+    const currentUser = AuthenticationAPI.getCurrentUser();
+    if (currentUser && chosenSubject && description && questionTitle && currentUser.email) {
+      try {
+        await QuestionsAPI.createQuestion(
+          questionTitle, 
+          description, 
+          chosenSubject,
+          currentUser.email);
+      } catch(e: any) {
+        console.log(e);
+      }
+      
+      setModalVisible(false);
+      setQuestionTitle("");
+      setChosenSubject("");
+      setDescription("");
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        avoidKeyboard={true}
+        isVisible={modalVisible}
+        onBackdropPress={() => setModalVisible(!modalVisible)}
+      >
+
+        <View style={styles.modalWindow}>
+          <Text style={styles.modalText}>Criação de Chat</Text>
+
+          <Input
+            value={questionTitle}
+            onChangeText={setQuestionTitle}
+            placeholder="Descreva sua dúvida em uma frase"
+            leftIcon={<MaterialIcons name="help" size={24} color="black" />}
+            autoCompleteType="none"
+          />
+
+          <Input
+            value={description}
+            onChangeText={setDescription}
+            placeholder="Descreva detalhadamente sua dúvida"
+            multiline={true}
+            leftIcon={<MaterialIcons name="mail" size={24} color="black" />}
+            autoCompleteType="none"
+          />
+
+          <DropDownPicker
+            placeholder="Matéria"
+            open={dropDrownExpanded}
+            value={chosenSubject}
+            items={availableSubjects}
+            setOpen={setDropDownExpanded}
+            setValue={setChosenSubject}
+            setItems={setAvailableSubjects}
+          />
+
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={createQuestion}
+          >
+            <Text style={styles.textStyle}>Enviar dúvida</Text>
+          </Pressable>
+        </View>
+      </Modal>
+
+      <FAB
+        icon={{ name: "add", color: "white" }}
+        placement="right"
+        onPress={() => setModalVisible(!modalVisible)}
+      />
+    </View> 
+  );
+}
 
 const styles = StyleSheet.create({
-  centeredView: { 
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: { 
-    width: "80%",
-    margin: 20,
+  modalWindow: {
+    width: "100%",
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
+    padding: 20,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -52,81 +141,3 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
 });
-
-export default function QuestionsListScreen() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [description, setDescription] = useState("");
-  const [questionTitle, setQuestionTitle] = useState("");
-  const [dropDrownExpanded, setDropDownExpanded] = useState(false);
-  const [chosenSubject, setChosenSubject] = useState("");
-  const [availableSubjects, setAvailableSubjects] = useState([
-    { label: "Matemática", value: "Math" },
-    { label: "Português", value: "Port" },
-    { label: "Geografia", value: "Geo" },
-    { label: "Biologia", value: "Bio" },
-    { label: "Química", value: "Chem" },
-    { label: "História", value: "Hist"},
-    { label: "Filosofia", value: "Philo"},
-    { label: "Sociologia", value: "Socio"},
-    { label: "Física", value: "Phys"},
-    { label: "Artes", value: "Arts"}
-  ])
-
-  return (
-    <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Criação de Chat</Text>
-
-            <Input
-              value={questionTitle}
-              onChangeText={setQuestionTitle}
-              placeholder="Descreva sua dúvida em uma frase"
-              leftIcon={<MaterialIcons name="help" size={24} color="black" />}
-              autoCompleteType="none"
-            />
-
-            <Input
-              value={description}
-              onChangeText={setDescription}
-              placeholder="Descreva detalhadamente sua dúvida"
-              multiline={true}
-              leftIcon={<MaterialIcons name="mail" size={24} color="black" />}
-              autoCompleteType="none"
-            />
-
-            <DropDownPicker
-              placeholder="Matéria"
-              open={dropDrownExpanded}
-              value={chosenSubject}
-              items={availableSubjects}
-              setOpen={setDropDownExpanded}
-              setValue={setChosenSubject}
-              setItems={setAvailableSubjects}
-            />
-
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-            >
-              <Text style={styles.textStyle}>Enviar dúvida</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-
-      <FAB
-        icon={{ name: "add", color: "white" }}
-        placement="right"
-        onPress={() => setModalVisible(!modalVisible)}
-      />
-    </View> 
-  );
-}
