cb083ba3eb74bdbecb78534ce92b3a6c0383cd4e
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-11-07 17:37:39 -0300

    feat: create ImageSelectorControl and implement selection deleting

--- src/views/addImage.tsx
+++ src/views/addImage.tsx
@@ -1,8 +1,134 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { Image, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import Swiper from "react-native-swiper";
+
+interface ImageSelectorControlProps {
+  onCancelClick: Function;
+  onDeleteClick: Function;
+  onPickImageClick: Function;
+  onSwiperIndexChange: Function;
+  selectedImagesURI: string[];
+  currentImageIndex: number;
+}
+
+const ImageSelectorControl: React.FC<ImageSelectorControlProps> = ({
+  onCancelClick,
+  onDeleteClick,
+  selectedImagesURI,
+  onSwiperIndexChange,
+  onPickImageClick,
+  currentImageIndex,
+}: any) => {
+  const swiperImageViews = useMemo(
+    () =>
+      selectedImagesURI.map((newImage: string, index: number) => (
+        <View
+          key={index}
+          style={{ width: "100%", height: "100%", shadowOpacity: 0 }}
+        >
+          <Image
+            source={{ uri: newImage }}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+      )),
+    [selectedImagesURI]
+  );
+  const controlBarImageViews = useMemo(
+    () =>
+      selectedImagesURI.map((newImage: string, index: number) => (
+        <View
+          key={index}
+          style={{ width: "19%", height: "100%", marginRight: "1.25%" }}
+        >
+          <Image
+            source={{ uri: newImage }}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+      )),
+    [selectedImagesURI]
+  );
+
+  const controlBarItems = useMemo(() => {
+    return controlBarImageViews.length > 5
+      ? controlBarImageViews
+      : [
+          ...controlBarImageViews,
+          <View
+            key={controlBarImageViews.length}
+            style={{
+              width: "19%",
+              height: "100%",
+              marginRight: "1.25%",
+              backgroundColor: "rgba(0,0,0,0.8)",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <MaterialIcons
+              name="add-to-photos"
+              size={40}
+              color="white"
+              onPress={onPickImageClick}
+            />
+          </View>,
+        ];
+  }, [controlBarImageViews, onPickImageClick]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar hidden />
+
+      <Swiper
+        loop={false}
+        onIndexChanged={onSwiperIndexChange}
+        index={currentImageIndex}
+        key={selectedImagesURI.length}
+      >
+        {swiperImageViews}
+      </Swiper>
+
+      <View
+        style={{ position: "absolute", top: 20, left: 20, shadowOpacity: 1 }}
+      >
+        <MaterialIcons
+          name="cancel"
+          size={40}
+          color="white"
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onPress={onCancelClick}
+        />
+      </View>
+
+      <View
+        style={{ position: "absolute", top: 20, right: 20, shadowOpacity: 1 }}
+      >
+        <MaterialIcons
+          name="restore-from-trash"
+          size={40}
+          color="white"
+          onPress={onDeleteClick}
+        />
+      </View>
+
+      <View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: "10%",
+          backgroundColor: "rgba(0,0,0,0.4)",
+          flexDirection: "row",
+        }}
+      >
+        {controlBarItems}
+      </View>
+    </View>
+  );
+};
 
 const ImagePickerExample = (props: any): JSX.Element => {
   // const openCamera = async (): Promise<void> => {
@@ -21,9 +147,29 @@ const ImagePickerExample = (props: any): JSX.Element => {
   //   }
   // };
 
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const [selectedImages, setSelectedImages] = useState<string[]>([]);
+
+  const onSwiperIndexChange = useCallback(
+    (index: number) => {
+      console.log("swiper index change", index);
+      setCurrentImageIndex(index);
+    },
+    [setCurrentImageIndex]
+  );
+
   const cancelSelection = async (): Promise<void> => {
     // TODO implement selection cancelation
-    console.log(displayItems);
+    console.log(selectedImages);
+  };
+  const deleteSelection = async (): Promise<void> => {
+    const nextImageIndex = currentImageIndex === 0 ? 0 : currentImageIndex - 1;
+
+    setSelectedImages(
+      selectedImages.filter((_, index) => index !== currentImageIndex)
+    );
+
+    setCurrentImageIndex(nextImageIndex);
   };
 
   const pickImage = useCallback(() => {
@@ -31,92 +177,20 @@ const ImagePickerExample = (props: any): JSX.Element => {
     console.log("oi");
   }, []);
 
-  
-  const displayImages = useMemo(
-    () =>
-      (props.route.params.images as string[]).map((newImage) => (
-        <View style={{ width: "19%", height: "100%", marginRight: "1.25%" }}>
-          <Image
-            source={{ uri: newImage }}
-            style={{ width: "100%", height: "100%" }}
-          />
-        </View>
-      )),
+  useEffect(
+    () => setSelectedImages(props.route.params.images),
     [props?.route?.params?.images]
   );
 
-  const displayItems = useMemo(() => {
-    return displayImages.length > 5
-      ? displayImages
-      : [
-          ...displayImages,
-          <View
-            style={{
-              width: "19%",
-              height: "100%",
-              marginRight: "1.25%",
-              backgroundColor: "rgba(0,0,0,0.8)",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <MaterialIcons
-              name="add-to-photos"
-              size={40}
-              color="white"
-              onPress={pickImage}
-            />
-          </View>,
-        ];
-  }, [displayImages]);
-
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar hidden />
-
-      <Swiper>
-        <Image
-          source={{ uri: props.route.params.images[0] }}
-          style={{ width: "100%", height: "100%", shadowOpacity: 0 }}
-        />
-
-        <Image
-          source={{ uri: props.route.params.images[1] }}
-          style={{ width: "100%", height: "100%", shadowOpacity: 0 }}
-        />
-      </Swiper>
-
-      <View
-        style={{ position: "absolute", top: 20, left: 20, shadowOpacity: 1 }}
-      >
-        <MaterialIcons
-          name="cancel"
-          size={40}
-          color="white"
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onPress={cancelSelection}
-        />
-      </View>
-
-      <View
-        style={{ position: "absolute", top: 20, right: 20, shadowOpacity: 1 }}
-      >
-        <MaterialIcons name="restore-from-trash" size={40} color="white" />
-      </View>
-
-      <View
-        style={{
-          position: "absolute",
-          bottom: 0,
-          width: "100%",
-          height: "10%",
-          backgroundColor: "rgba(0,0,0,0.4)",
-          flexDirection: "row",
-        }}
-      >
-        {displayItems}
-      </View>
-    </View>
+    <ImageSelectorControl
+      onCancelClick={cancelSelection}
+      onDeleteClick={deleteSelection}
+      onPickImageClick={pickImage}
+      onSwiperIndexChange={onSwiperIndexChange}
+      selectedImagesURI={selectedImages}
+      currentImageIndex={currentImageIndex}
+    />
   );
 };
 
