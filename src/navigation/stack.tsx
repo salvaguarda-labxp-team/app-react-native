eccff2aefb337138d5459d7256e9928962c4b02f
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-11-25 09:42:37 -0300

    fix: removed unused ChatList screen from routing

--- src/navigation/stack.tsx
+++ src/navigation/stack.tsx
@@ -18,9 +18,16 @@ const Stack = createNativeStackNavigator();
 export default (): JSX.Element => (
   <Stack.Navigator initialRouteName="Login">
     <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-    <Stack.Screen name="Register" component={RegisterScreen} />
-    <Stack.Screen name="ChatList" component={ChatsListScreen} />
+    <Stack.Screen
+      options={{ title: "Esqueci minha senha" }}
+      name="ForgotPassword"
+      component={ForgotPasswordScreen}
+    />
+    <Stack.Screen
+      name="Register"
+      options={{ title: "Cadastro" }}
+      component={RegisterScreen}
+    />
     <Stack.Screen
       name="Menu"
       component={Menu}
@@ -29,6 +36,7 @@ export default (): JSX.Element => (
     <Stack.Screen name="Chat" component={ChatScreen} />
     <Stack.Screen
       name="Add Image"
+      options={{ title: "Coletar imagens" }}
       component={AddImage}
       options={{ headerShown: false, gestureEnabled: false }}
     />
--- src/views/Menu.tsx
+++ src/views/Menu.tsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { QuestionsListScreen, ChatsListScreen } from ".";
+import { QuestionsListScreen } from ".";
 
 const Drawer = createDrawerNavigator();
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-  },
-});
 
 export default function Menu() {
   return (
     <Drawer.Navigator initialRouteName="Conversas">
-      <Drawer.Screen name="QuestionsList" component={QuestionsListScreen} />
-      <Drawer.Screen name="ChatsList" component={ChatsListScreen} />
+      <Drawer.Screen options={{title: "Dúvidas"}} name="QuestionsList" component={QuestionsListScreen} />
     </Drawer.Navigator>
   );
 }
